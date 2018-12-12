d03f33032a55f79059bac796684fef67279ea8c1
AstrobioMike <leemd@usc.edu>
2018-12-11 18:40:53 -0800

    building up primary script and testing dir

--- test_data/clean_after_test.sh
+++ test_data/clean_after_test.sh
@@ -1,1 +1,1 @@
-rm -rf FINAL_aligned_SCGs* *.tmp *tmpdir Genbank_files_with_no_CDSs.txt NCBI_accessions_not_found.txt Genbank_genomes_summary_info.tsv NCBI_genomes_summary_info.tsv Fasta_genomes_summary_info.tsv All_genomes_summary_info.tsv
+rm -rf FINAL_aligned_SCGs* *.tmp *tmpdir Genbank_files_with_no_CDSs.txt NCBI_* Genbank_genomes_summary_info.tsv Fasta_genomes_summary_info.tsv All_genomes_summary_info.tsv rm *.gz
