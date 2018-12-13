24cedb703fd9ffbdcd6eebcd886b38ede2e750ff
AstrobioMike <leemd@usc.edu>
2018-12-12 18:24:38 -0800

    mod

--- test_data/clean_after_test.sh
+++ test_data/clean_after_test.sh
@@ -1,1 +1,1 @@
-rm -rf FINAL_aligned_SCGs* *.tmp *tmpdir Genbank_files_with_no_CDSs.txt NCBI_* Genbank_genomes_summary_info.tsv Fasta_genomes_summary_info.tsv All_genomes_summary_info.tsv rm *.gz
+rm -rf FINAL_aligned_SCGs* *.tmp *tmpdir Genbank_files_with_no_CDSs.txt NCBI_* Genbank_genomes_summary_info.tsv Fasta_genomes_summary_info.tsv All_genomes_summary_info.tsv rm *.gz Genomes_removed_for_too_few_hits.tsv
