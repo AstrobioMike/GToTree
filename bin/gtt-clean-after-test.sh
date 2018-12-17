c374e4189fbf0597b537237188267666cf9af497
AstrobioMike <leemd@usc.edu>
2018-12-17 15:30:03 -0800

    mod cleanup script

--- bin/gtt-clean-after-test.sh
+++ bin/gtt-clean-after-test.sh
@@ -1,1 +1,3 @@
-rm -rf FINAL_aligned_SCGs* *.tmp *tmpdir Genbank_files_with_no_CDSs.txt NCBI_* Genbank_genomes_summary_info.tsv Fasta_genomes_summary_info.tsv All_genomes_summary_info.tsv *.gz Genomes_removed_for_too_few_hits.tsv Redundant_input_accessions.txt Missing_IDs.txt
+rm -rf FINAL_aligned_SCGs* *.tmp *tmpdir Genbank_files_with_no_CDSs.txt NCBI_* Genbank_genomes_summary_info.tsv 
+rm -rf Fasta_genomes_summary_info.tsv All_genomes_summary_info.tsv *.gz Genomes_removed_for_too_few_hits.tsv 
+rm -rf Redundant_input_accessions.txt Missing_IDs.txt *.ssi
