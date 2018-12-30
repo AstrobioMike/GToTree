cff03e704218a1e4595a0f0ecfdab114dce2f4b2
AstrobioMike <leemd@usc.edu>
2018-12-30 10:38:43 -0800

    mod

--- conda-setup.sh
+++ conda-setup.sh
@@ -30,14 +30,13 @@ export PATH=\"$(pwd)/bin:"'$PATH'\"" \
 
 export GToTree_HMM_dir=\"$(pwd)/hmm_sets\"" >> ${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh
 
-printf "\n    ${GREEN}Setting up taxonkit...${NC}\n\n"
-
 ## downloading ncbi tax database for taxonkit and setting variable
 mkdir -p ncbi_tax_info
 cd ncbi_tax_info
 
-curl --retry 10 -O ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
-tar -xzvf taxdump.tar.gz
+curl --silent --retry 10 -O ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
+
+tar -xzf taxdump.tar.gz
 rm taxdump.tar.gz
 
 echo "export TAXONKIT_DB=$(pwd)" >> ${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh
