6a0d7b2b63b0c599b12b14526d5d9ab83248eb7c
AstrobioMike <leemd@usc.edu>
2019-01-05 14:47:12 -0800

    putting in conda version check for setup script

--- conda-setup.sh
+++ conda-setup.sh
@@ -5,6 +5,17 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
+## checking that user conda version is at least 4.5.10 to avoid problems
+cur_conda_version=$(conda --version | conda --version | cut -f2 -d " ")
+too_old_version=4.5.0
+
+if [ "$(printf '%s\n' "$cur_conda_version" "$too_old_version" | sort -V | head -n1)" != "$too_old_version" ]; then
+    printf "\n    ${RED}It seems your conda version ($cur_conda_version) needs to be updated :(${NC}\n\n"
+    printf "    Please update it with \`conda update conda\` and then try again.\n\n\n"
+    printf "Exiting for now.\n\n"
+    return
+fi
+
 printf "\n    ${GREEN}Setting up conda environment...${NC}\n\n"
 
 ## adding conda channels
