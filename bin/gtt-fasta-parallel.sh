a1c32a92e5d0e652c972640b82514f22f7706459
AstrobioMike <leemd@usc.edu>
2019-01-16 19:12:27 -0800

    adding conservative mode

--- bin/gtt-fasta-parallel.sh
+++ bin/gtt-fasta-parallel.sh
@@ -10,6 +10,7 @@ hmm_file=$3
 num_cpus=$4
 hmm_target_genes_total=$5
 output_dir=$6
+conservative_mode=$7
 
 # setting assembly name as filename with no extension
 assembly="$(basename ${1%.*})"
@@ -35,6 +36,12 @@ do
     grep -w -c "$SCG" ${tmp_dir}/${assembly}_curr_hmm_hits.tmp
 done > ${tmp_dir}/${assembly}_uniq_counts.tmp
 
+## make list here of only those present in exactly 1 copy if running in "conservative mode" ("-C" flag specified)
+if [ $conservative_mode == "true" ]; then
+    paste ${tmp_dir}/uniq_hmm_names.tmp ${tmp_dir}/${assembly}_uniq_counts.tmp > ${tmp_dir}/${assembly}_conservative_filtering_counts_tab.tmp
+    awk -F "\t" ' $2 == 1 ' ${tmp_dir}/${assembly}_conservative_filtering_counts_tab.tmp | cut -f 1 > ${tmp_dir}/${assembly}_conservative_target_unique_hmm_names.tmp
+fi
+
 ## adding SCG-hit counts to table
 paste <(printf $assembly) <(printf %s "$(cat ${tmp_dir}/${assembly}_uniq_counts.tmp | tr "\n" "\t")") >> ${output_dir}/All_genomes_SCG_hit_counts.tsv
 
@@ -56,11 +63,24 @@ printf "$assembly\t$1\t$taxid\t$num_SCG_hits\t$perc_comp_rnd\t$perc_redund_rnd\n
 ### Pulling out hits for this genome ###
 # looping through ribosomal proteins and pulling out each first hit (hmm results tab is sorted by e-value):
 esl-sfetch --index ${tmp_dir}/${assembly}_genes.tmp > /dev/null
-        
-for SCG in $(cat ${tmp_dir}/uniq_hmm_names.tmp)
-do
-    grep -w "$SCG" ${tmp_dir}/${assembly}_curr_hmm_hits.tmp | awk '!x[$3]++' | cut -f1 -d " " | esl-sfetch -f ${tmp_dir}/${assembly}_genes.tmp - | sed "s/>.*$/>$assembly/" | sed 's/^Usage.*$//' | sed 's/^To see.*$//' | sed '/^$/d' >> ${tmp_dir}/${SCG}_hits.faa
-done
+
+# if conservative mode is on, then only pulling genes that were identified in exactly 1 copy
+if [ $conservative_mode  == "true" ]; then
+
+    for SCG in $(cat ${tmp_dir}/${assembly}_conservative_target_unique_hmm_names.tmp)
+    do
+        grep -w "$SCG" ${tmp_dir}/${assembly}_curr_hmm_hits.tmp | awk '!x[$3]++' | cut -f1 -d " " | esl-sfetch -f ${tmp_dir}/${assembly}_genes.tmp - | sed "s/>.*$/>$assembly/" | sed 's/^Usage.*$//' | sed 's/^To see.*$//' | sed '/^$/d' >> ${tmp_dir}/${SCG}_hits.faa
+    done
+
+# if conservative mode not on, taking best hit
+else
+
+    for SCG in $(cat ${tmp_dir}/uniq_hmm_names.tmp)
+    do
+        grep -w "$SCG" ${tmp_dir}/${assembly}_curr_hmm_hits.tmp | awk '!x[$3]++' | cut -f1 -d " " | esl-sfetch -f ${tmp_dir}/${assembly}_genes.tmp - | sed "s/>.*$/>$assembly/" | sed 's/^Usage.*$//' | sed 's/^To see.*$//' | sed '/^$/d' >> ${tmp_dir}/${SCG}_hits.faa
+    done
+
+fi
 
 rm -rf ${tmp_dir}/${assembly}_*.tmp ${tmp_dir}/${assembly}_genes.tmp.ssi
 
