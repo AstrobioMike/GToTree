b18f43847e3d3731b0d7d95e45a69b4a2c3c4861
AstrobioMike <michael.lee0517@gmail.com>
2019-07-24 07:28:52 -0400

    mod test script, bump version, mod clean after test script

---
+++ bin/gtt-test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# setting colors to use
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+printf "\n"
+
+printf "  ${GREEN}Your test data directory is located here:\n${NC}"
+printf "    $TEST_DATA_DIR\n\n"
+
+sleep 1
+
+printf "  ${GREEN}Running test as: \n${NC}"
+printf "    GToTree -a ${TEST_DATA_DIR}/ncbi_accessions.txt "'\\ \n'
+printf "            -g ${TEST_DATA_DIR}/genbank_files.txt "'\\ \n'
+printf "            -f ${TEST_DATA_DIR}/fasta_files.txt "'\\ \n'
+printf "            -A ${TEST_DATA_DIR}/amino_acid_files.txt "'\\ \n'
+printf "            -m ${TEST_DATA_DIR}/genome_to_id_map.tsv "'\\ \n'
+printf "            -p ${TEST_DATA_DIR}/pfam_targets.txt "'\\ \n'
+printf "            -H Universal -t -j 4 -o GToTree_test\n\n"
+
+sleep 2
+
+GToTree -a ${TEST_DATA_DIR}/ncbi_accessions.txt -g ${TEST_DATA_DIR}/genbank_files.txt -f ${TEST_DATA_DIR}/fasta_files.txt -A ${TEST_DATA_DIR}/amino_acid_files.txt -H Universal -m ${TEST_DATA_DIR}/genome_to_id_map.tsv -p ${TEST_DATA_DIR}/pfam_targets.txt -t -j 4 -o GToTree_test
