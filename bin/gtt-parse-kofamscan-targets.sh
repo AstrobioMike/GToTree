bf2c7511549f7a5c874157b2e59ec4d7deab1db8
AstrobioMike <michael.lee0517@gmail.com>
2022-02-11 22:18:02 -0800

    working on adding KO searching support like we have for PFams

---
+++ bin/gtt-parse-kofamscan-targets.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+target_KOs_file=$1
+output_dir=$2
+
+full_KO_list_file="${KO_data_dir}/ko_list"
+full_KO_hmms_dir="${KO_data_dir}/profiles"
+
+sub_KO_list_file="${output_dir}/KO_searching/target-KOs.tsv"
+sub_KO_hmms_dir="${output_dir}/KO_searching/target-KO-profiles/"
+
+mkdir -p ${sub_KO_hmms_dir}
+
+# making target ko_list file and copying target hmms to working area
+head -n 1 ${full_KO_list_file} > ${sub_KO_list_file}
+
+for ko in $(cat ${target_KOs_file}); do
+    grep -m 1 -w "^${ko}" ${full_KO_list_file} >> ${sub_KO_list_file}
+    cp ${full_KO_hmms_dir}/${ko}.hmm ${sub_KO_hmms_dir}
+done
