fa7cd19eb51aaf19dd0bd8858cbc02f56aaec029
AstrobioMike <michael.lee0517@gmail.com>
2022-10-10 11:28:56 -0700

    updating gtt-get-additional-pfam-targets.sh to work again after pfam shifted over to being hosted by interpro

--- bin/gtt-get-additional-pfam-targets.sh
+++ bin/gtt-get-additional-pfam-targets.sh
@@ -3,30 +3,37 @@
 tmp_dir=${1}
 output_dir=${2}
 
-base_link="https://pfam.xfam.org/family/"
+# base_link="https://pfam.xfam.org/family/"
+    # base link updated Oct-2022 when pfam hosting shifted to interpro
+base_link="https://www.ebi.ac.uk/interpro/wwwapi/entry/pfam/"
 
 # starting table of what was requested and what was found (version-wise, the latest is always pulled from Pfam when downloading as below)
 printf "requested_Pfam\tpulled_Pfam\n" > ${output_dir}/Pfam_search_results/info/Requested-and-pulled.tsv
 
-for target in $(cat ${tmp_dir}/uniq_pfam_targets.tmp)
+for initial_target in $(cat ${tmp_dir}/uniq_pfam_targets.tmp)
 do
 
-    # --insecure flag added on 29-Nov-2020, due to pfam certificate being invalid (https://github.com/AstrobioMike/GToTree/issues/28)
-    curl --insecure --silent --retry 10 -o ${tmp_dir}/${target}.hmm "${base_link}${target}/hmm"
 
+    # getting target without version specified if there was one (including a version doesn't work anymore since pfam-hosting shifted to interpro)
+    target=$(echo ${initial_target} | cut -f 1 -d ".")
+
+    # --insecure flag added on 29-Nov-2020, due to pfam certificate being invalid (https://github.com/AstrobioMike/GToTree/issues/28)
+    curl --insecure --silent --retry 10 -o ${tmp_dir}/${target}.hmm.gz "${base_link}${target}?annotation=hmm"
+    gunzip ${tmp_dir}/${target}.hmm.gz
+    
     if [ -s ${tmp_dir}/${target}.hmm ]; then
         # getting accession pulled (to account for current version on Pfam as compared to what was searched)
         actual_target=$(grep -m1 "^ACC" ${tmp_dir}/${target}.hmm | tr -s " " "\t" | cut -f 2)
         printf "$actual_target\n" >> ${tmp_dir}/actual_pfam_targets.tmp
 
-        if [ $target != $actual_target ]; then
+        if [ $initial_target != $actual_target ]; then
             mv ${tmp_dir}/${target}.hmm ${tmp_dir}/${actual_target}.hmm
         fi
 
         cat ${tmp_dir}/${actual_target}.hmm >> ${tmp_dir}/all_pfam_targets.hmm
 
         # adding searched and pulled to info table (meaning which versions of a Pfam)
-        printf "${target}\t${actual_target}\n" >> ${output_dir}/Pfam_search_results/info/Requested-and-pulled.tsv
+        printf "${initial_target}\t${actual_target}\n" >> ${output_dir}/Pfam_search_results/info/Requested-and-pulled.tsv
 
     else # aborting if any of the pfam targets couldn't be pulled successfully
         printf "\n  ${RED}One of the target Pfams could not be successfully downloaded :(${NC}\n"
